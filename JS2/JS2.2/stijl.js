3080abb73276e3b335914eab95245c06f50e2707
Mansoor <mansoor1999@live.com>
2021-02-18 20:30:11 +0100

    JS2 klaar

---
+++ JS2/JS2.2/stijl.js
@@ -0,0 +1,7 @@
+document.getElementById("bl1").style.cssText = "background-Color:cyan; text-align:center; padding-top:10px;";
+document.getElementById("bl2").style.cssText = "background-Color:green; text-align:center; margin-top:10px;";
+document.getElementById("bl3").style.cssText = "background-Color:yellow; text-align:right; margin-top:30px;";
+document.getElementById("bl4").style.cssText = "background-Color:red";
+
+
+
